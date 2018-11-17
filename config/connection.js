a3053d58ed9c49c3749035e993e0dfcac21012ac
Max Smith <maxsmith216@gmail.com>
2018-11-16 21:03:26 -0800

    heroku connection

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,18 @@ var connection = mysql.createConnection({
   database: "netflix_db"
 });
 
+if (process.env.JAWSDB_DB) {
+  connection = mysql.createConnection(process.env.JAWSDB_DB);
+} else {
+  connection = mysql.createConnection({
+    host: 'uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    username: 'm5zeesxgs2q8i8nb',
+    password: 'u2em2ml5w894ykno',
+    database: 'i0f3dht71zfkm6aj'
+  });
+};
+
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
