7141199bc29b1e522ea7f0fa33da50c583ae5054
Max Smith <maxsmith216@gmail.com>
2018-11-18 11:05:18 -0800

    uppdated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,24 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "netflix_db"
-});
+var connection 
 
 if (process.env.JAWSDB_DB) {
   connection = mysql.createConnection(process.env.JAWSDB_DB);
 } else {
   connection = mysql.createConnection({
-    host: 'uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    username: 'm5zeesxgs2q8i8nb',
-    password: 'u2em2ml5w894ykno',
-    database: 'i0f3dht71zfkm6aj'
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'netflix_db'
   });
 };
 
-
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error("error connecting: " + err.stack + '\n\n');
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("connected as id " + connection.threadId + '\n\n');
 });
 
 module.exports = connection;
